8a8ceab7a5d28762a39e70c4b2c380d244be834e
Alex <alexkoshernosiegov@gmail.com>
2018-07-09 12:21:30 +0300

    Управление стилем

--- application/src/components/pages/Authentication/Authentication.vue
+++ application/src/components/pages/Authentication/Authentication.vue
@@ -63,7 +63,7 @@ export default {}
     </div>
 </template>
 
-/* Здесь имеется элемент div с классом l-auth-container, который выполняет роль контейнера. Следом идёт ещё один div с классом l-auth, который содержит структуру элементов для организации формы ввода, в частности, это элемент v-form, привязанный к данным переменной validLogin.
+<!-- Здесь имеется элемент div с классом l-auth-container, который выполняет роль контейнера. Следом идёт ещё один div с классом l-auth, который содержит структуру элементов для организации формы ввода, в частности, это элемент v-form, привязанный к данным переменной validLogin.
 
 Внутри него находится пара подписанных полей ввода v-text-field, которые привязаны к данным из credentials (мы займёмся этими данными ниже). Поля снабжены иконками, взятыми из https://material.io/icons/, с ними также связаны правила по проверке ввода (и там и там — одни и те же правила, не будем усложнять проект), кроме того, оба эти поля являются обязательными.
 
@@ -73,4 +73,74 @@ export default {}
 
 Следующий фрагмент кода описывает структуру формы регистрации в системе, которая видна лишь в том случае, если переменная signUpVisible установлена в true. Устройство этой формы похоже на устройство формы входа в систему, тут изменены лишь несколько строк. В частности, здесь используется переменная signUpPasswordVisible вместо loginPasswordVisible и другой метод обработки щелчка по кнопке.
 
-Далее, тут имеется панель v-snackbar, которая, в ходе аутентификации, используется для вывода сообщений. */
+Далее, тут имеется панель v-snackbar, которая, в ходе аутентификации, используется для вывода сообщений. -->
+
+<script>
+    import Authentication from '@/components/pages/Authentication'
+    export default {
+        data () {
+            return {
+                snackbar: false;
+                validLogin: false,
+                validSignUp: false,
+                signUpVisible: false,
+                loginPasswordVisible: false,
+                signUpPasswordVisible: false,
+                rules: [ (value) => !!value || 'This field is required' ],
+                credentials: {
+                    username: '',
+                    password: ''
+                },
+                newUser: {
+                    username: '',
+                    password: ''
+                },
+                message: ''
+            }
+        },
+        methods: {
+            submitAuthentication () {
+                Authentication.authenticate(this, this.credentials, '/')
+            },
+
+            submitSignUp () {
+                Authentication.signup(this, this.newUser, '/')
+            }
+        }
+    }
+    
+</script>
+<!-- snackbar: используется для панели сообщений.
+validLogin: используется для проверки формы входа в систему.
+validSignUp: используется для проверки формы регистрации.
+signUpVisible: используется для вывода формы регистрации (при установке в true).
+loginPasswordVisible: указывает на то, может ли пользователь видеть пароль, вводимый в форме входа в систему.
+signUpPasswordVisible: указывает на то, можно ли видеть пароль, вводимый в форме регистрации.
+rules: правила проверки данных, введённых в поля форм.
+credentials: объект, привязанный к полям ввода формы входа в систему, используемый для аутентификации пользователя.
+newUser: объект, привязанный к полям ввода формы регистрации в системе.
+message: используется для вывода сообщений в ходе аутентификации. -->
+
+<!-- В этом компоненте есть пара методов. В методе submitAuthentication осуществляется вызов метода authenticate из файла Authentication, с передачей контекста, учётных данных и пути для перенаправления. Метод submitSignUp используется для вызова метода signup.
+-->
+<style lang="scss">
+  @import "./../../../assets/styles";
+.l-auth {
+    background-color: $background-color;
+    padding: 15px;
+    margin: 45px auto;
+    min-width: 272px;
+    max-width: 320px;
+    animation: bounceIn 1s forwards ease;
+  }
+.l-signup {
+    background-color: $background-color;
+    padding: 15px;
+    margin: 45px auto;
+    min-width: 272px;
+    max-width: 320px;
+    animation: slideInFromLeft 1s forwards ease;
+  }
+</style> 
+
+<!-- код стилизации компонента -->
