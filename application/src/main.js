0c5c376a762c077c1344de2886ddbe3a5df57be8
Alex <alexkoshernosiegov@gmail.com>
2018-07-09 11:25:35 +0300

    Фикс ошибки из примера: был пропущен import Vue

--- application/src/main.js
+++ application/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
 import VueCookie from 'vue-cookie'
 import Vuetify from 'vuetify'
 import('../node_modules/vuetify/dist/vuetify.min.css')
