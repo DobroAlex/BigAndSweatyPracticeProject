4ef8ccc5a98e4a09e6531de98379b6be1a4750b8
Alex <alexkoshernosiegov@gmail.com>
2018-07-09 11:24:02 +0300

    Работа над фоном и кастомизацией

--- application/src/App.vue
+++ application/src/App.vue
@@ -10,3 +10,24 @@ export default {
   name: 'app'
 }
 </script> /*Newline required at end of file http://eslint.org/docs/rules/eol-last*/
+//импортируем ранее подготовленные стили scss и задаём использование фиксированного фонового изображения для приложения. Мы стремимся к тому, чтобы на любом устройстве экран приложения выглядел примерно одинаково.
+
+<style lang = "scss">
+  @import "./assets/styles.scss";
+  body
+  {
+    background: url('./assets/images/background.jpg') no-repeat center center fixed;
+    &:after  //В псевдоэлементе body :after мы задаём параметр background-color, записывая в него значение переменной $background-tint. Это позволит применить к фоновому изображению фуксивый цветной фильтр. 
+    {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
+</style>
