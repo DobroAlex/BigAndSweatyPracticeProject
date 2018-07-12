b88c2a7cbdc664703c0e690da97947de979a129f
Alex <alexkoshernosiegov@gmail.com>
2018-07-12 11:21:30 +0300

    Файлы автоматической установки депенсов для npm

---
+++ troubleshooting scripts/chromeANDsassproblem.bat
@@ -0,0 +1,11 @@
+cd ..
+cd application
+npm install -g del@3.0.0
+npm install -g extract-zip@1.6.7
+npm install -g kew@0.7.0
+npm install -g mkdirp@0.5.1
+npm install -g request@2.87.0
+npm install -g chromedriver@2.40.0
+npm install -g sass@1.9.1 
+npm install -g node-sass@4.9.2
+npm -i
---
+++ troubleshooting scripts/chromeANDsassproblem.sh
@@ -0,0 +1,13 @@
+cd ..
+cd application 
+echo "Next steps may require sudo or root priveledge!\n"
+read -n 1 -s -r -p "Press any key to continue"
+sudo npm install -g del@3.0.0
+sudo npm install -g extract-zip@1.6.7
+sudo npm install -g kew@0.7.0
+sudo npm install -g mkdirp@0.5.1
+sudo npm install -g request@2.87.0
+sudo npm install -g chromedriver@2.40.0
+sudo npm install -g sass@1.9.1 
+sudo npm install -g node-sass@4.9.2
+sudo npm i
