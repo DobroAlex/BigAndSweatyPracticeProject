a87f1d750a6d84b184f101a7ae786b5c460821bf
Alex <alexkoshernosiegov@gmail.com>
2018-07-12 13:30:04 +0300

    Новые пакеты

--- troubleshooting scripts/chromeANDsassproblem.bat
+++ troubleshooting scripts/chromeANDsassproblem.bat
@@ -1,11 +1,13 @@
 cd ..
 cd application
-npm install -g del@3.0.0
-npm install -g extract-zip@1.6.7
-npm install -g kew@0.7.0
-npm install -g mkdirp@0.5.1
-npm install -g request@2.87.0
-npm install -g chromedriver@2.40.0
-npm install -g sass@1.9.1 
-npm install -g node-sass@4.9.2
-npm -i
+npm install  del@3.0.0
+npm install  extract-zip@1.6.7
+npm install  kew@0.7.0
+npm install  mkdirp@0.5.1
+npm install  request@2.87.0
+npm install  chromedriver@2.40.0
+npm install  sass@1.9.1 
+npm install  node-sass@4.9.2
+npm install sinon@6.1.3
+npm install karma-sinon-chai@2.0.2
+npm i
--- troubleshooting scripts/chromeANDsassproblem.sh
+++ troubleshooting scripts/chromeANDsassproblem.sh
@@ -2,12 +2,14 @@ cd ..
 cd application 
 echo "Next steps may require sudo or root priveledge!\n"
 read -n 1 -s -r -p "Press any key to continue"
-sudo npm install -g del@3.0.0
-sudo npm install -g extract-zip@1.6.7
-sudo npm install -g kew@0.7.0
-sudo npm install -g mkdirp@0.5.1
-sudo npm install -g request@2.87.0
-sudo npm install -g chromedriver@2.40.0
-sudo npm install -g sass@1.9.1 
-sudo npm install -g node-sass@4.9.2
+sudo npm install  del@3.0.0
+sudo npm install  extract-zip@1.6.7
+sudo npm install  kew@0.7.0
+sudo npm install  mkdirp@0.5.1
+sudo npm install  request@2.87.0
+sudo npm install  chromedriver@2.40.0
+sudo npm install  sass@1.9.1 
+sudo npm install  node-sass@4.9.2
+sudo npm install sinon@6.1.3
+sudo npm install karma-sinon-chai@2.0.2
 sudo npm i
